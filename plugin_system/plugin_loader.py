f7ca2b4a7149e522baa29aa52ca4c9777785dc0f
JOrosco2 <JOrosco2@gmail.com>
2025-07-07 16:22:01 -0700

    Update to discover plugins:
    Now looks for packages instead of individual plugins

--- plugin_system/plugin_loader.py
+++ plugin_system/plugin_loader.py
@@ -4,17 +4,21 @@ import pkgutil
 
 
 #build a list of available plugins from the plugin folder. Default search value is power meter
-def discover_plugins(folder="plugins", plugin_type="power_meter"):
+def discover_plugins(package="plugin_system.plugins", plugin_type="power_meter"):
     plugins = []
-    for _, module_name, _ in pkgutil.iter_modules([folder]):
-        print(f"Checking the following: {folder}.{module_name}")
-        mod = importlib.import_module(f"{folder}.{module_name}")
+
+    package_module = importlib.import_module(package)
+    package_path = package_module.__path__
+
+    for _, module_name, _ in pkgutil.iter_modules(package_path):
+        print(f"Checking: {package}.{module_name}")
+        mod = importlib.import_module(f"{package}.{module_name}")
         if hasattr(mod, "plugin_info") and mod.plugin_info.get("type") == plugin_type:
             cls_name = mod.plugin_info["class"]
             cls = getattr(mod,cls_name)
             plugins.append((module_name,cls))
     if len(plugins) == 0:
-        print(f"ERROR Could not find any plugins of type: {plugin_type}")
+        print(f"ERROR Could not find any plugins of type: {plugin_type} in {package}")
     return plugins
 
 #method for automaticlly connecting devices. User can sepcify number of a type of device to detect
